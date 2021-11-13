4631ef2154a0d50dab9d5929e20917f786f0d2dd
tony.yoo <l22491360@gmail.com>
2021-11-13 18:58:41 +0900

    add setup codes

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 #!bin/bash
+import setuptools
+
+setuptools.setup(
+    name='mipack',
+    version='0.0.1',
+    author='swyo',
+    author_email='l22491360@gmail.com',
+    description='My implemented packages for python',
+    url='https://github.com/swyo/mipack',
+    install_requires=[],
+    keywords=['python', 'packaging'],
+    python_requires='>=3.9',
+    packages=setuptools.find_packages(exclude=('docs')),
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
