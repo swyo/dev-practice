852b37d80d24575e5e36ce328314eb8f7e5c65c3
tony.yoo <l22491360@gmail.com>
2021-11-13 19:33:51 +0900

    version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='mipack',
-    version='0.0.1',
+    version='0.0.2',
     author='swyo',
     author_email='l22491360@gmail.com',
     description='My implemented packages for python',
