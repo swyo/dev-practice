acb2d3ad9a82ada98523ad5119219a0029072f89
tony.yoo <l22491360@gmail.com>
2021-11-13 20:15:12 +0900

    version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fin:
 
 setuptools.setup(
     name='mipack',
-    version='0.0.4',
+    version='0.0.5',
     author='swyo',
     author_email='l22491360@gmail.com',
     description='My implemented packages for python',
